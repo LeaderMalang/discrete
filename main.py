c6cc83c3f605783db42ba6876efbe831bfc1f154
Brian Muthui <brian.muthui@andela.com>
2019-03-17 20:38:02 +0300

    Add function docs

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ logging.basicConfig(format='%(asctime)s: %(message)s', level=logging.INFO)
 complexType = np.csingle if config["inputBytesPerValue"] == "4" else np.complex64
 floatType = np.float32 if config["inputBytesPerValue"] == "4" else np.float64
 
+'''
+Reads values from an input file and calculates and returns
+the discrete fast fourier transform of the values
+'''
 def getAccelTraceDFFT(inputfile):
   try:
     accelTrace = np.fromfile(inputfile, dtype=float)
@@ -16,20 +20,35 @@ def getAccelTraceDFFT(inputfile):
   dffTAccelTrace = np.fft.fft(accelTrace)
   return dffTAccelTrace
 
+'''
+Calculates and returns mean of given array of values
+'''
 def getMean(values):
   return np.mean(values, None, complexType)
 
+'''
+Calculates and returns median of given array of values
+'''
 def getMedian(values):
   return np.median(values)
 
+'''
+Calculates and returns standard deviation of given array of values
+'''
 def getStandardDeviation(values):
   return np.std(values, None, complexType)
 
+'''
+Logs mean, median and standard deviation of given array of values to stdout
+'''
 def logStatistics(values):
   logging.info('Mean - %s', getMean(values))
   logging.info('Median - %s', getMedian(values))
   logging.info('Standard Deviation - %s', getStandardDeviation(values))
 
+'''
+Writes given array of values to output file specified in config
+'''
 def writeValuesToFile(values):
   outputfile = open(config["outputfile"], "w+b")
   values.tofile(outputfile)
